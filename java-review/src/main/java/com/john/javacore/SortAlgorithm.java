0f83c3b9d502790c9e4076e48c58e4ef2acf3012
git-john <kangqiang999@163.com>
2019-04-08 17:46:37 +0800

    show me code

--- java-review/src/main/java/com/john/javacore/SortAlgorithm.java
+++ java-review/src/main/java/com/john/javacore/SortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.john.javacore;
 
+
+import com.sun.istack.internal.NotNull;
+
 import java.util.Arrays;
 
 /**
@@ -25,8 +28,7 @@ public class SortAlgorithm {
     }
 
     //冒泡排序， 两两比较，将最大的数放到最后面
-    public static void bubbleSort(){
-        int[] nums = new int[]{3,9,5,2,8,6,7,15,10};
+    public static void bubbleSort(@NotNull int[] nums){
         for(int i=1; i<nums.length; i++){
             for(int j=0; j<nums.length-i; j++){
                 if(nums[j] > nums[j+1]){
@@ -37,14 +39,10 @@ public class SortAlgorithm {
             }
         }
 
-        for(int i : nums){
-            System.out.printf("%d,", i);
-        }
     }
 
     //选择排序： 找出最小数的索引放到排序序列的起始位置，再从剩余数组里面找最小数放到排序序列的末尾，当剩余数组为1的时候即结束
-    public static void selectionSort(){
-        int[] nums = new int[]{3,9,5,2,8,6,7,15,10};
+    public static void selectionSort(int nums[]){
         for(int i=0; i<nums.length; i++){
             int minIndex = i;
             for(int j=i; j<nums.length; j++){
@@ -56,12 +54,85 @@ public class SortAlgorithm {
             nums[i] = nums[minIndex];
             nums[minIndex] = temp;
         }
-        for(int i : nums){
-            System.out.printf("%d,", i);
+    }
+
+    //插入排序：对于未排序数据，在已排序序列中从后向前扫描，找到对应位置后插入数据
+    public static void insertSort(int[] nums){
+        int current;
+        for(int i=0; i<nums.length-1; i++){
+            current = nums[i+1];
+            int preindex = i;
+            while (preindex>=0 && current<nums[preindex]){
+                nums[preindex+1] = nums[preindex];
+                preindex--;
+            }
+            nums[preindex+1] = current;
         }
     }
 
+    /**
+     * 希尔排序
+     *
+     * @param array
+     * @return
+     */
+    public static int[] ShellSort(int[] array) {
+        int len = array.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && array[preIndex] > temp) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return array;
+    }
+
+    /**
+     * 归并排序
+     * @param array
+     * @return
+     */
+    public static int[] mergeSort(int[] array){
+        if(array.length<2) return array;
+
+        int mid = array.length/2;
+        int[] left = Arrays.copyOfRange(array, 0, mid); //取值范围为[from, to)
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+        return merge(mergeSort(left),mergeSort(right));
+    }
+
+    private static int[] merge(int[] left, int[] right){
+        int i=0,j=0;
+        int newLength = left.length + right.length;
+        int[] result = new int[newLength];
+        for(int index=0; index<newLength; index++){
+            if(i>=left.length)
+                result[index] = right[j++];
+            else if(j>=right.length)
+                result[index] = left[i++];
+            else if(left[i]<right[j])
+                result[index] = left[i++];
+            else
+                result[index] = right[j++];
+        }
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
-        selectionSort();
+        int[] nums = new int[]{3,9,5,2,8,6,7,15,10};
+        int[] result = mergeSort(nums);
+        for(int i : result){
+            System.out.printf("%d,", i);
+        }
+
     }
 }
