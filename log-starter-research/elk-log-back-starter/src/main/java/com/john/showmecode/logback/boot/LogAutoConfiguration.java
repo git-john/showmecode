22e4e8e6309d45d0fa27c434b954925e2a6bca86
john <kangqiang999@163.com>
2019-06-20 19:11:25 +0800

    导入starter，进行相应配置

---
+++ log-starter-research/elk-log-back-starter/src/main/java/com/john/showmecode/logback/boot/LogAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.john.showmecode.logback.boot;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: kangq
+ * @Date: 2019/6/20 19:08
+ */
+@Configuration
+@EnableConfigurationProperties(LogbackProperties.class)
+public class LogAutoConfiguration {
+}
--- log-starter-research/elk-logback-app/src/main/java/com/john/showmecode/elklogbackapp/Controller/IndexController.java
+++ log-starter-research/elk-logback-app/src/main/java/com/john/showmecode/elklogbackapp/Controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.john.showmecode.elklogbackapp.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/helloworld")
     public String helloWorld(){
+        logger.info("============= hello world ============");
         return "hello world";
     }
 }
