431792d2d728d85e9a544d08789c4f2175ccb0d2
john <kangqiang999@163.com>
2019-08-08 15:54:19 +0800

    demo

--- trmitools/src/main/java/com/john/showmecode/tools/ToolsApplication.java
+++ trmitools/src/main/java/com/john/showmecode/tools/ToolsApplication.java
@@ -18,10 +18,18 @@ public class ToolsApplication {
         doSsh();
     }
 
+    /**
+     * 需要目标服务器的ssh配置允许，否则auth fail。
+     * PermitRootLogin 设置成yes
+     * GSSAPIAuthentication 修改为 no
+     * UseDNS yes 修改为 no
+     * StrictModes设置成no
+     * @throws Exception
+     */
     private static void doSsh() throws Exception{
         JSch jSch = new JSch();
-        Session session = jSch.getSession("root","68.4.50.206",22);
-        session.setPassword("WHZAdmin2019");
+        Session session = jSch.getSession("root","xx.xx.xx.xx",22);
+        session.setPassword("sdfdsa");
         Properties config = new Properties();
         config.put("StrictHostKeyChecking", "no");
         session.setConfig(config);
