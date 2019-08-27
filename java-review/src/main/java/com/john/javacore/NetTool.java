95463eabe959869f7b2e519d9572164617757b83
john <kangqiang999@163.com>
2019-08-27 11:46:56 +0800

    do something

---
+++ java-review/src/main/java/com/john/javacore/NetTool.java
@@ -0,0 +1,12 @@
+package main.java.com.john.javacore;
+
+/**
+ * @Author: kangq
+ * @Date: 2019/8/6 15:32
+ */
+public class NetTool {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
